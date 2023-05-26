c5b40b779571f3a9a5cbe6857101a7278042305d
Cody Measner <YOURGITHUBEMAILHEREINQUOTES>
2023-05-26 13:46:03 -0500

    update

--- parallax/js/script.js
+++ parallax/js/script.js
@@ -1,29 +1,32 @@
-setInterval(showTime, 1000);
-function showTime() {
-    let time = new Date();
-    let hour = time.getHours();
-    let min = time.getMinutes();
-    let sec = time.getSeconds();
-    am_pm = "AM";
+
+const statusDisplay = document.querySelector('.game--status');
+
+let gameActive = true;
+
+let currentPlayer = "X";
+
+let gameState = ["", "", "", "", "", "", "", "", ""];
+
+const winningMessage = () => `Player ${currentPlayer} has won!`;
+const drawMessage = () => `Game ended in a draw!`;
+const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
  
-    if (hour > 12) {
-        hour -= 12;
-        am_pm = "PM";
-    }
-    if (hour == 0) {
-        hr = 12;
-        am_pm = "AM";
-    }
- 
-    hour = hour < 10 ? "0" + hour : hour;
-    min = min < 10 ? "0" + min : min;
-    sec = sec < 10 ? "0" + sec : sec;
- 
-    let currentTime = hour + ":"
-            + min + ":" + sec + am_pm;
- 
-    document.getElementById("clock")
-            .innerHTML = currentTime;
+statusDisplay.innerHTML = currentPlayerTurn();
+function handleCellPlayed() {
+
 }
+function handlePlayerChange() {
 
+}
+function handleResultValidation() {
 
+}
+function handleCellClick() {
+
+}
+function handleRestartGame() {
+
+}
+ 
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
+document.querySelector('.game--restart').addEventListener('click', handleRestartGame);
